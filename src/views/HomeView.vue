1dd92f7a272fc0a4d44d9e36203702c0ff5699c6
kevin92422 <kevin92422@gmail.com>
2024-03-19 17:36:30 +0000

    add raw html components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <header>
+    <h1>台北掛我電話</h1>
+  </header>
   <main>
-    <TheWelcome />
+    <button>心理測驗</button>
+    <p>
+      快來參加一場語音的沉浸之旅，獲得測驗結果一起解鎖可愛的電話吉祥物之外，還可以匹配到和你性格相近的藝人喔
+    </p>
+    <p>開始你的語音之旅</p>
+    <button>聲音彩蛋</button>
+    <p>內有12個由驚喜來賓錄製的小語音，快來找找你偶像的聲音，聽完6個回答問題還可以參加抽獎喔!!</p>
+    <p>快來抽獎</p>
   </main>
+  <footer>
+    <button>Home</button>
+    <button>心理測驗</button>
+    <button>聲音彩蛋</button>
+  </footer>
 </template>
