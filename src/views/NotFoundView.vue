e3223d850a88b755dce409c8a2a39ced73f7dd0c
littlechin <littlechintw@gmail.com>
2024-03-23 17:02:10 +0800

    Add 404 voice

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="NotFound">
+  <div class="NotFound" @touchstart="playSound" @mouseover="playSound" @touchmove="playSound">
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div class="text-center">
         <p class="text-base font-semibold text-indigo-600">404</p>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl rainbow-text">Page not found</h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl rainbow-text">您撥的號碼是空號，請查明後再撥</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">The number you dialed is incorrect, please check it and try
+          again!</p>
       </div>
     </main>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+let audio = ref(new Audio('404NotFound.mp3'));
+
+const playSound = async () => {
+  await audio.value.play();
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
