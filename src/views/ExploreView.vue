fd06f038058d874397beb367b52c9253f4d5eefd
littlechin <littlechintw@gmail.com>
2024-04-15 00:42:38 +0800

    Update problem page

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -18,7 +18,22 @@
       </div>
 
       <div class="testPage" v-else>
-        <p class="text-black text-xl max-w-[250px]">Start test...</p>
+        <div class="bg-gradient-to-br from-white to-[#F6F800] h-screen flex flex-col items-center justify-center">
+          <div class="flex justify-center items-center">
+            <div class="relative bg-black w-80 h-64 rounded-3xl flex items-center justify-center">
+              <p class="absolute top-0 mt-1 text-white text-xl max-w-[250px] text-center">NTU Art Fest</p>
+
+              <div class="relative bg-[#C7D8C9] w-64 h-48 rounded-3xl overflow-hidden">
+                <img src="@/assets/explore-page/screen.png"
+                  class="absolute w-full h-full transform scale-150 opacity-20" />
+                <p
+                  class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black text-xl w-9/10 text-center">
+                  當你還沒熟悉完村莊，電話又響了，你穿越到了一個未來的科技世界，城市裡光彩奪目的高樓大廈林立，人們穿梭在各種高科技裝置之間。一位科學家邀請你參觀他的實驗室，向你展示了一個令人驚嘆的發明，你覺得很有趣的地方可能是？
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </transition>
   </div>
