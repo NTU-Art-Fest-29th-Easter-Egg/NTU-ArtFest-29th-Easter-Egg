2f3c654ff10eaf4c7007b3174ee4e6ca192a3245
garyellow <gary20011110@gmail.com>
2024-04-16 00:35:50 +0800

    Add psychological banner component and update router configuration

---
+++ src/components/psychologicalBanner.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-gradient-to-br from-white to-[#F6F800] h-screen">
+    <img src="@/assets/title.png" class="w-5/12 px-5 py-3" />
+    <RouterView></RouterView>
+  </div>
+</template>
