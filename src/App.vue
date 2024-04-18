520f010cf39c3a530c391cb6b85ba0d76876b14c
garyellow <gary20011110@gmail.com>
2024-04-19 01:00:51 +0800

    Refactor router ori

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { onBeforeMount } from 'vue'
-import { RouterView } from 'vue-router'
-import router from './router'
+import { RouterView, useRouter } from 'vue-router'
 
 onBeforeMount(() => {
+  const router = useRouter()
   if (window.location.pathname.toLowerCase() != '/sharewithqrcode' && window.matchMedia('(min-width: 768px)').matches) {
     router.push('/shareWithQRCode')
   }
