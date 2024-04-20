3b92abb2f99b07095be6940e53598a6150f9e025
littlechintw <littlechintw@gmail.com>
2024-04-20 00:57:14 +0000

    littlechintw published a site update

--- assets/ResultView-CgebABln.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a as c}from"./index-21IB4ZBj.js";const o={},a={class:"result"},n=c("h1",null,"This is an result page",-1),_=[n];function r(i,l){return s(),t("div",a,_)}const u=e(o,[["render",r]]);export{u as default};
--- assets/psychologicalBanner-MWeBnZx6.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,f as s,o as t,c,g as n,a as r}from"./index-21IB4ZBj.js";const a="/assets/title-ChYzw3bu.png",_={},i={class:"bg-gradient-to-br from-white to-[#F6F800] h-screen"},l=r("img",{src:a,class:"w-5/12 px-5 py-3"},null,-1);function p(d,m){const e=s("RouterView");return t(),c("div",i,[l,n(e)])}const h=o(_,[["render",p]]);export{h as default};
