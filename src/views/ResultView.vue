c92aaad3b0cf63d4d32b24eb962bd6c0e24b20d7
garyellow <gary20011110@gmail.com>
2024-04-20 13:21:22 +0800

    Add some instruction

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,48 +1,78 @@
 <template>
-  <div class="mx-8 h-[calc(85dvh-52px)] flex justify-center items-center">
+  <div class="mx-8 h-[calc(85dvh-52px)] flex flex-col justify-center items-center">
     <img :src="resultSrc" class="max-h-[70dvh]" />
+    <div class="text-base animate-pulse" :class="isSupported ? 'text-yellow-700' : 'text-red-700'">
+      {{ isSupported ? '長按圖片可以下載' : '內嵌瀏覽器無法長按下載' }}
+    </div>
   </div>
   <div class="fixed bottom-[calc(12dvh-20px)] right-6">
-    <button
-      class="text-xl text-white mr-1 hover:text-yellow-400 font-bold h-10 w-16 bg-black text-center rounded-2xl hover:scale-105"
-      @click="share()"
+    <ElButton
+      color="#000000"
+      size="large"
+      round
+      :disabled="!isSupported"
+      class="*:text-lg *:text-center *:font-bold *:text-white *:hover:text-yellow-500"
+      @click="shareResult()"
     >
       分享
-    </button>
-    <button class="h-10 w-40 ml-1 bg-black rounded-2xl hover:scale-105">
-      <a
-        href="https://ntuartfest.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-xl text-white hover:text-yellow-400 text-center font-bold"
-        >回到藝術季官網</a
-      >
-    </button>
+    </ElButton>
+    <ElButton
+      color="#000000"
+      size="large"
+      round
+      tag="a"
+      href="https://ntuartfest.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="*:text-lg *:text-center *:font-bold *:text-white *:hover:text-yellow-500"
+    >
+      回到藝術季官網
+    </ElButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 import allHash from '@/assets/explore-page/result_hash.json'
 import { useShare, type UseShareOptions } from '@vueuse/core'
-import { onMounted, ref } from 'vue'
+import { onBeforeMount, onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
+import { ElButton, ElMessage } from 'element-plus'
 
 const route = useRoute()
 const router = useRouter()
 
-const hash = route.hash.slice(1)
 const resultSrc = ref('')
 const options = ref<UseShareOptions>({})
 
-const { share } = useShare(options)
+const { isSupported, share } = useShare(options)
 
-onMounted(async () => {
+const shareResult = async () => {
+  if (isSupported.value) {
+    await share()
+  }
+}
+
+onBeforeMount(async () => {
+  const hash = route.hash.slice(1)
   if (allHash.result.every((item) => item.hash !== hash)) {
     router.push({ path: '/404' })
   }
 
   resultSrc.value = (await import(`@/assets/results/${hash}.png`)).default
-  const image = await fetch(resultSrc.value).then((res) => res.blob())
-  options.value.files = [new File([image], 'result.png', { type: image.type })]
+
+  if (isSupported.value) {
+    const image = await fetch(resultSrc.value).then((res) => res.blob())
+    options.value.files = [new File([image], 'result.png', { type: image.type })]
+  }
+})
+
+onMounted(() => {
+  if (!isSupported.value) {
+    ElMessage.warning({
+      message: '內嵌瀏覽器無法長按下載及分享',
+      center: true,
+      duration: 7000
+    })
+  }
 })
 </script>
