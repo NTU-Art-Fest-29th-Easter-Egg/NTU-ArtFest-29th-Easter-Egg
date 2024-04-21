470cac2ec5f4d94d57780e25acfeba7f3325e07e
garyellow <gary20011110@gmail.com>
2024-04-21 13:52:00 +0800

    Refactor psychologicalBanner.vue component and update routing in index.ts

--- src/components/psychologicalBanner.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="bg-gradient-to-br from-white to-[#F6F800] h-lvh">
-    <img src="@/assets/title.png" class="w-48 pl-5 pt-3" />
-    <RouterView></RouterView>
-  </div>
-</template>
