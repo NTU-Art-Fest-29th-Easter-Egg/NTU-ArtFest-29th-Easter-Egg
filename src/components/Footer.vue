d5f00a211c7e95f9dce0afa67783de56e35d19d7
littlechin <littlechintw@gmail.com>
2024-04-21 16:40:28 +0800

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-black h-auto flex flex-col justify-center items-center mt-4 mb-4">
+        <img src="@/assets/footer/logo.png" class="w-[40%] mb-2">
+        <a href="https://ntuartfest.com" target="_blank" rel="noopener noreferrer"
+            class="text-white underline">藝術季官網</a>
+    </div>
+</template>
