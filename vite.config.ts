1f01cfcc2b90155f505ac69f14fad31a91dc8018
Yong Cyun Huang <littlechintw@gmail.com>
2025-05-09 16:30:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NTU-ArtFest-29th-Easter-Egg/',
   plugins: [
     vue(),
     VueDevTools(),
